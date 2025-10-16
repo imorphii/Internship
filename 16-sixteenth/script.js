fd6f7d4386ef3e94b03c098422f5bdd7e2e9d4e8
imorphii <morphiiii.77@gmail.com>
2025-10-16 12:04:43 +0330

    start JS and make a simple JS file

---
+++ 16-sixteenth/script.js
@@ -0,0 +1,14 @@
+const nameOne = "ali";
+const nameTwo = "mahdi";
+const nameThree = "amir";
+const nameFour = "mania";
+const nameFive = "masoomeh";
+console.log(nameFour);
+
+
+
+let names =["ali","mahdi","milad"];
+console.log(names);
+names[3]="amir"
+console.log(names);
+
