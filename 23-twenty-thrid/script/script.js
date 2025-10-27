023c9b3c3654cd09ea711b87e806916a2aaed8e1
imorphii <morphiiii.77@gmail.com>
2025-10-27 15:45:05 +0330

    part 2 of ALIBABA project| add footer and litte javascript

---
+++ 23-twenty-thrid/script/script.js
@@ -0,0 +1,7 @@
+
+
+
+
+function otherService() {
+    window.open("http://127.0.0.1:5500/alibab-login/login-page.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
