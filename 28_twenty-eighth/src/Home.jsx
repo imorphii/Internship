a7ba4c5bc0ea9e46eec532e9347a88a9dcb4223f
imorphii <morphiiii.77@gmail.com>
2025-11-11 09:03:07 +0330

    Frist exercise of React

---
+++ 28_twenty-eighth/src/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+
+            <h1>hello dude!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores magnam exercitationem temporibus esse cum nihil fugiat officiis nobis neque corporis, nostrum sapiente quis officia, non ut voluptas doloribus, sit voluptates.</p>
+            
+            <Footer />
+        </div>
+    )
+}
